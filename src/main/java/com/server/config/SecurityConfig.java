bc1b7b64138be90121e3d6e00a459adba95246f0
tutqq96 <tutqq96@gmail.com>
2024-09-19 22:48:23 +0700

    feat: Allow swagger

--- src/main/java/com/server/config/SecurityConfig.java
+++ src/main/java/com/server/config/SecurityConfig.java
@@ -65,6 +65,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(
                         "/cars/all",
                         "/cars/**",
+                        "//api-docs/**",
+                        "//api-docs",
                         "/v3/api-docs/**",
                         "/swagger-ui/**",
                         "/swagger-ui.html").permitAll()
